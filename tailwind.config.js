b85cd51338698fb6d206cccf037f9c7f633c9f20
EmekaAgara <emekaagara@gmail.com>
2022-05-17 22:49:48 +0100

    padding

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    './public/index.html',
+    './src/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100:'#E2E2D5',
+          200:'#888883',
+        } 
+      },
+    },
+  },
+  plugins: [],
+}
